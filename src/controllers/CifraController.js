518d448ff95a93e329282a46f0519d90d2014861
Ramon Rodrigues <ramonrodsouza@gmail.com>
2024-07-31 18:24:07 -0300

    criando Crud Cifras

--- src/controllers/CifraController.js
+++ src/controllers/CifraController.js
@@ -2,26 +2,76 @@ import Cifra from "../models/Cifra"
 
 class CifraController {
 
- async store(req, res){
+  async index(req, res) {
+    const cifras = await Cifra.find()
+    return res.json(cifras)
+  }
 
-  console.log(req.body)
+  async store(req, res) {
+
+    console.log(req.body)
+
+    const { title, tom, singer, Struct, description, createdAt } = req.body
+    const { user_id } = req.headers
+
+    let cifra = await Cifra.findOne({ title, singer })
+
+    if (!cifra) {
+      cifra = await Cifra.create({
+        user: user_id,
+        title,
+        tom,
+        singer,
+        Struct,
+        description,
+        createdAt
+      })
+
+    }
+    return res.json(cifra)
+  }
+
+  async update(req, res) {
+    const { cifra_id } = req.params
+    const { title, tom, singer, Struct, description } = req.body
+    const { user_id } = req.headers
+    const cifra = await Cifra.updateOne({ _id: cifra_id }, {
+      user: user_id,
+      title,
+      tom,
+      singer,
+      Struct,
+      description
+    })
+
+    return res.send()
+  }
+
+  async destroy(req, res) {
+
+    const { cifra_id } = req.params
+
+    if(!cifra_id) {
+      return res.status(400).json({ message: 'ID da cifra é necessário' });
+    }
+
+    const cifra = await Cifra.findById(cifra_id)
+
+    if (!cifra) {
+      return res.status(404).json({ message: 'Cifra não encontrada' })
+    }
+
+    await Cifra.findByIdAndDelete({ _id: cifra_id })
+
+    return res.json({ message: 'Cifra Excluída com sucesso!' })
+
+  }
   
-  const { title, tom, singer, Struct, description, createdAt } = req.body
-  const { user_id } = req.headers
-
-  const cifra = await Cifra.create({ 
-    user: user_id,
-    title, 
-    tom, 
-    singer, 
-    Struct, 
-    description, 
-    createdAt 
-   })
-
-  return res.json(cifra)
- }
 }
+
+
+
+
 
 export default new CifraController
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,11 @@ import CifraController from './controllers/CifraController'
 const routes = new Router()
 
 routes.post('/sessions', SessionController.store)
+
 routes.post('/cifras', CifraController.store)
+routes.get('/cifras', CifraController.index)
+routes.put('/cifras/:cifra_id', CifraController.update)
+routes.delete('/cifras/delete/:cifra_id', CifraController.destroy)
+
 
 export default routes 
